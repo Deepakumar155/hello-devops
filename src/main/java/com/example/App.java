c0cfdc1b4df23e1cae847e6b58c4fc9193a95ea2
Deepak <deepak1552005@gmail.com>
2026-08-13 18:47:25 +0000

    update application to HTTP server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,35 @@
+
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Deepak!" );
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(
+                new InetSocketAddress(8081), 0
+        );
+
+        server.createContext("/", (HttpExchange exchange) -> {
+
+            String response = "Hello Deepak! CI/CD is working!";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            try (OutputStream output = exchange.getResponseBody()) {
+                output.write(response.getBytes());
+            }
+        });
+
+        server.start();
+
+        System.out.println("Server started on port 8080");
     }
 }
+
